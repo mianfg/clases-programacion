99f899b1c2375ce7d5f00f6b62725d188c37cf21
mianfg <mianfg19@gmail.com>
2018-09-03 12:46:24 +0200

    Añadidos ejemplos

---
+++ Ejemplos/tema4-ejemplo4.1.cpp
@@ -0,0 +1,24 @@
+/*
+ *  EJERCICIO 4.1 - diasHasta()
+ *    Declaración y llamada a una función: días desde fecha hasta hoy
+ *
+ *  Clases particulares de programación en C++
+ *  Autor: Miguel Ángel Fernández Gutiérrez (@mianfg)
+ *  Fecha: 3 septiembre, 2018
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int diasHasta(int dia, int mes, int anio) {
+    // ¡intenta implementar tú esta función!
+    return 7; // siempre devolverá esto, es simplemente para usarlo como ejemplo
+}
+
+int main() {
+    int dia, mes, anio;
+    cout << "Inserte fecha: ";
+    cin >> dia >> mes >> anio;
+    cout << "Días desde esa fecha hasta hoy: " << diasHasta(dia, mes, anio) << endl;
+}
---
+++ Ejemplos/tema4-ejemplo4.2.cpp
@@ -0,0 +1,20 @@
+/*
+ *  EJERCICIO 4.2 - buenosDias()
+ *    Una función que te da los buenos días (sin importar si es de noche)
+ *
+ *  Clases particulares de programación en C++
+ *  Autor: Miguel Ángel Fernández Gutiérrez (@mianfg)
+ *  Fecha: 3 septiembre, 2018
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void buenosDias() {
+    cout << "¡¡Buenos días!! :)" << endl;
+}
+
+int main() {
+    buenosDias();
+}
